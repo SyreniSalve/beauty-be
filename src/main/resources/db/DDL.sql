6042e17198bd3266de756a7e14f5e7b351b6c7af
Evaldas Kvietkauskas <efkapc@gmail.com>
2022-03-26 15:11:46 +0200

    commit of DDL and DML updates

--- src/main/resources/db/DDL.sql
+++ src/main/resources/db/DDL.sql
@@ -1,1 +1,21 @@
 -- DDL
+CREATE TABLE `user` (
+                        `id` bigint PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                        `email` varchar(20) NOT NULL UNIQUE,
+                        `password` varchar(120) NOT NULL,
+                        `username` varchar(20) NOT NULL UNIQUE
+);
+
+CREATE TABLE `role` (
+                        `id` bigint PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                        `role` varchar(20) UNIQUE
+);
+
+CREATE TABLE `user_role` (
+                             `user_id` bigint,
+                             `role_id` bigint
+);
+
+ALTER TABLE `user` ADD FOREIGN KEY (`id`) REFERENCES `user_role` (`user_id`);
+
+ALTER TABLE `role` ADD FOREIGN KEY (`id`) REFERENCES `user_role` (`role_id`);
--- src/main/resources/db/DML.sql
+++ src/main/resources/db/DML.sql
@@ -1,1 +1,19 @@
 -- DML
+INSERT INTO user (id, email, password) VALUES ('1', 'adminmail@mail.com', 'adminpassword');
+INSERT INTO user (id, email, password) VALUES ('2', 'owner1mail@mail.com', 'owner1password');
+INSERT INTO user (id, email, password) VALUES ('3', 'owner2mail@mail.com', 'owner2password');
+INSERT INTO user (id, email, password) VALUES ('4', 'owner3mail@mail.com', 'owner3password');
+INSERT INTO user (id, email, password) VALUES ('5', 'owner4mail@mail.com', 'owner4password');
+INSERT INTO user (id, email, password) VALUES ('6', 'usermail@mail.com', 'userpassword');
+
+INSERT INTO role (id, role) VALUES ('1', 'admin');
+INSERT INTO role (id, role) VALUES ('2', 'owner');
+INSERT INTO role (id, role) VALUES ('3', 'user');
+
+INSERT INTO user_role (user_id, role_id) VALUES ('1', '1');
+INSERT INTO user_role (user_id, role_id) VALUES ('2', '2');
+INSERT INTO user_role (user_id, role_id) VALUES ('3', '2');
+INSERT INTO user_role (user_id, role_id) VALUES ('4', '2');
+INSERT INTO user_role (user_id, role_id) VALUES ('5', '2');
+INSERT INTO user_role (user_id, role_id) VALUES ('6', '3');
+
