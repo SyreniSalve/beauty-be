5cef2e316e6df8d21288823bb4f1cef965ef88ff
Evaldas Kvietkauskas <efkapc@gmail.com>
2022-03-27 09:25:53 +0300

    commit of DDL and DML updates

--- src/main/resources/db/DDL.sql
+++ src/main/resources/db/DDL.sql
@@ -1,24 +1,20 @@
 -- DDL
-CREATE TABLE `user` (
-                        `id` bigint PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                        `email` varchar(250) NOT NULL UNIQUE,
-                        `password` varchar(120) NOT NULL,
-                        `username` varchar(50) NOT NULL UNIQUE
-                         FOREIGN KEY (`id`) REFERENCES `user_role` (`user_id`),
+CREATE TABLE user (
+    `id` bigint PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `email` varchar(250) NOT NULL UNIQUE,
+    `password` varchar(120) NOT NULL,
+    `username` varchar(50) NOT NULL UNIQUE
 );
 
-CREATE TABLE `role` (
-                        `id` bigint PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                        `role` varchar(20) UNIQUE
-                         FOREIGN KEY (`id`) REFERENCES `user_role` (`role_id`),
+CREATE TABLE role (
+    `id` bigint PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `role` varchar(20) UNIQUE
 );
 
 CREATE TABLE `user_role` (
                              `user_id` bigint NOT NULL ,
                              `role_id` bigint NOT NULL,
-                             UNIQUE(user_id, role_id)
+                             UNIQUE(user_id, role_id),
+                             FOREIGN KEY (`user_id`) REFERENCES user (`id`),
+                             FOREIGN KEY (`role_id`) REFERENCES role (`id`)
 );
-
---ALTER TABLE `user` ADD FOREIGN KEY (`id`) REFERENCES `user_role` (`user_id`);
-
---ALTER TABLE `role` ADD FOREIGN KEY (`id`) REFERENCES `user_role` (`role_id`);
--- src/main/resources/db/DML.sql
+++ src/main/resources/db/DML.sql
@@ -5,5 +5,5 @@ INSERT INTO role (role) VALUES ('ROLE_ADMIN');
 INSERT INTO role (role) VALUES ('ROLE_OWNER');
 INSERT INTO role (role) VALUES ('ROLE_USER');
 
-INSERT INTO user_role (user_id, role_id) VALUES ('SELECT id FROM user WHERE username='admininistrator'', 'SELECT id FROM role WHERE role='ROLE_ADMIN'');
+INSERT INTO user_role (user_id, role_id) VALUES ((SELECT id FROM user WHERE username='administrator'), (SELECT id FROM role WHERE role='ROLE_ADMIN'));
 
