7b800853947a0425c9ce8c1451b11c28ce105eab
Evaldas Kvietkauskas <efkapc@gmail.com>
2022-03-27 13:45:23 +0300

    commit of DDL and DML updates with token

--- src/main/resources/db/DDL.sql
+++ src/main/resources/db/DDL.sql
@@ -1,20 +1,33 @@
 -- DDL
-CREATE TABLE user (
-    `id` bigint PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `email` varchar(250) NOT NULL UNIQUE,
-    `password` varchar(120) NOT NULL,
-    `username` varchar(50) NOT NULL UNIQUE
+CREATE TABLE user
+(
+    `id`       bigint PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `email`    varchar(250)       NOT NULL UNIQUE,
+    `password` varchar(120)       NOT NULL,
+    `username` varchar(50)        NOT NULL UNIQUE
 );
 
-CREATE TABLE role (
-    `id` bigint PRIMARY KEY NOT NULL AUTO_INCREMENT,
+CREATE TABLE role
+(
+    `id`   bigint PRIMARY KEY NOT NULL AUTO_INCREMENT,
     `role` varchar(20) UNIQUE
 );
 
-CREATE TABLE `user_role` (
-    `user_id` bigint NOT NULL ,
+CREATE TABLE `user_role`
+(
+    `user_id` bigint NOT NULL,
     `role_id` bigint NOT NULL,
-    UNIQUE(user_id, role_id),
+    UNIQUE (user_id, role_id),
     FOREIGN KEY (`user_id`) REFERENCES user (`id`),
     FOREIGN KEY (`role_id`) REFERENCES role (`id`)
 );
+
+CREATE TABLE `refresh_token`
+(
+    `id`          bigint PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `expiry_date` datetime(6)        NOT NULL,
+    `token`       varchar(255)       NOT NULL UNIQUE,
+    `user_id`     bigint             NOT NULL,
+    FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
--- src/main/resources/db/DML.sql
+++ src/main/resources/db/DML.sql
@@ -1,9 +1,13 @@
 -- DML
-INSERT INTO user (email, password,username) VALUES ('adminmail@mail.com', 'encriptedadminpassword','administrator');
+INSERT INTO user (email, password, username)
+VALUES ('adminmail@mail.com', '$2a$10$y5ynLMW3jNqMy1g9e3xMxuwc4iwIdzhwSx4LEJLYno8pV5qQ8OgSO', 'administrator');
 
-INSERT INTO role (role) VALUES ('ROLE_ADMIN');
-INSERT INTO role (role) VALUES ('ROLE_OWNER');
-INSERT INTO role (role) VALUES ('ROLE_USER');
+INSERT INTO role (role)
+VALUES ('ROLE_ADMIN');
+INSERT INTO role (role)
+VALUES ('ROLE_OWNER');
+INSERT INTO role (role)
+VALUES ('ROLE_USER');
 
-INSERT INTO user_role (user_id, role_id) VALUES ((SELECT id FROM user WHERE username='administrator'), (SELECT id FROM role WHERE role='ROLE_ADMIN'));
-
+INSERT INTO user_role (user_id, role_id)
+VALUES ((SELECT id FROM user WHERE username = 'administrator'), (SELECT id FROM role WHERE role = 'ROLE_ADMIN'));
