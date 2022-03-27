8455b90d2b6aaee5620ee45ece99ab7c189eb407
Evaldas Kvietkauskas <efkapc@gmail.com>
2022-03-27 09:01:52 +0300

    commit of DDL and DML updates

--- src/main/resources/db/DML.sql
+++ src/main/resources/db/DML.sql
@@ -1,9 +1,9 @@
 -- DML
 INSERT INTO user (email, password,username) VALUES ('adminmail@mail.com', 'encriptedadminpassword','administrator');
 
-INSERT INTO role (role) VALUES ('ROLE_admin');
-INSERT INTO role (role) VALUES ('ROLE_owner');
-INSERT INTO role (role) VALUES ('ROLE_user');
+INSERT INTO role (role) VALUES ('ROLE_ADMIN');
+INSERT INTO role (role) VALUES ('ROLE_OWNER');
+INSERT INTO role (role) VALUES ('ROLE_USER');
 
-INSERT INTO user_role (user_id, role_id) VALUES ('SELECT id FROM user WHERE username='admininistrator'', 'SELECT id FROM role WHERE role='admin'');
+INSERT INTO user_role (user_id, role_id) VALUES ('SELECT id FROM user WHERE username='admininistrator'', 'SELECT id FROM role WHERE role='ROLE_ADMIN'');
 
