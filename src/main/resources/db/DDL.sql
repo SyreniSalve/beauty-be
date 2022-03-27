ec3dee39c80c918753f092c86f931219689ee6c7
Evaldas Kvietkauskas <efkapc@gmail.com>
2022-03-27 08:56:14 +0300

    commit of DDL and DML updates

--- src/main/resources/db/DDL.sql
+++ src/main/resources/db/DDL.sql
@@ -4,18 +4,21 @@ CREATE TABLE `user` (
                         `email` varchar(250) NOT NULL UNIQUE,
                         `password` varchar(120) NOT NULL,
                         `username` varchar(50) NOT NULL UNIQUE
+                         FOREIGN KEY (`id`) REFERENCES `user_role` (`user_id`),
 );
 
 CREATE TABLE `role` (
                         `id` bigint PRIMARY KEY NOT NULL AUTO_INCREMENT,
                         `role` varchar(20) UNIQUE
+                         FOREIGN KEY (`id`) REFERENCES `user_role` (`role_id`),
 );
 
 CREATE TABLE `user_role` (
-                             `user_id` bigint NOT NULL,
-                             `role_id` bigint NOT NULL
+                             `user_id` bigint NOT NULL ,
+                             `role_id` bigint NOT NULL,
+                             UNIQUE(user_id, role_id)
 );
 
-ALTER TABLE `user` ADD FOREIGN KEY (`id`) REFERENCES `user_role` (`user_id`);
+--ALTER TABLE `user` ADD FOREIGN KEY (`id`) REFERENCES `user_role` (`user_id`);
 
-ALTER TABLE `role` ADD FOREIGN KEY (`id`) REFERENCES `user_role` (`role_id`);
+--ALTER TABLE `role` ADD FOREIGN KEY (`id`) REFERENCES `user_role` (`role_id`);
